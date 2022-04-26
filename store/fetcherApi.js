b5198a918bbda11d64fce578d9c259b078d250a6
Ojo Paul <tosinkoa1@gmail.com>
2022-04-26 22:37:09 +0100

    Pushing the frontend

--- store/fetcherApi.js
+++ store/fetcherApi.js
@@ -3,7 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const fetcherApi = createApi({
   reducerPath: "fetcherApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/",
+    // baseUrl: "http://localhost:5000/",
+    baseUrl: "https://big-c-backend.herokuapp.com/",
     credentials: "include",
   }),
   tagTypes: ["ForCar", "UserData"],
